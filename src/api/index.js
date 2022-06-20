a804ad90a2d74ee57258c2d33343f7d1d45fa3aa
abhinesh g <abhineshg04@gmail.com>
2022-06-20 13:03:42 +0530

    Third (3rd) message

--- src/api/index.js
+++ src/api/index.js
@@ -5,3 +5,15 @@ export const readTodos=()=>axios.get("http://localhost:5000/api/gets/");
 export const createTodos=newTodo=>axios.post(url,newTodo);
 export const updateTodo=(id,updatedTodo) =>axios.patch(`${url}/${id}`,updatedTodo);
 export const deleteTodo=(id) =>axios.delete(`${url}/${id}`);
+
+// 1. git init
+
+// 2. git add .
+
+// 3. git status
+
+// 4. git commit -m 'your message'
+
+// 5. git remote add origin 'your_url_name' 
+
+// 6. git push -u origin master //then login w/ your creds
